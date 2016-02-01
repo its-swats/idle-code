b73d8d96d170f983f47f9fff10c64a6800740a60
Shawn Watson <shawnw86@gmail.com>
2016-01-31 21:06:41 -0800

    Add functional game loop

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,8 +6,8 @@ export const Main = React.createClass({
     return (
       <div><h1>Idle Coder</h1>
       <p>{this.props.code}</p>
-      <p>{this.props.player}</p>
-      <p>{this.props.player.maxCode}</p>
+      <p>{this.props.player['codeSpent']}</p>
+      <p>{this.props.player.totalSpeed}</p>
       </div>
     )
   }
@@ -16,7 +16,8 @@ export const Main = React.createClass({
 function mapStateToProps(state) {
   return {
     code: state.get('code'),
-    player: state.getIn(['player'])
+    player: state.getIn(['player']).toObject(),
+    upgrades: state.getIn(['upgrades']).toObject
   };
 }
 
