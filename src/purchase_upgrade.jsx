fbbe6f0ee0b661eb0acac0be0be1c4bbbc3116db
Shawn Watson <shawnw86@gmail.com>
2016-02-17 23:29:35 -0800

    Update purchaseUpgrade to update all player stats on purchase

--- src/purchase_upgrade.jsx
+++ src/purchase_upgrade.jsx
@@ -1,8 +1,17 @@
 export function purchaseUpgrade(state, item) {
-	if (state.get("code") >= state.getIn(["upgrades", "purchasables", item, "cost"])) {
-		return state.updateIn(["upgrades", "purchasables", item, "owned"], val => val + 1);
+	let cost = state.getIn(["upgrades", "purchasables", item, "cost"])
+	if (state.get("code") >= cost) {
+		return state.withMutations(state => {
+			state.updateIn(["player", "codeSpent"], val => val + cost);
+			state.updateIn(["upgrades", "purchasables", item, "owned"], val => val + 1);
+			state.updateIn(["upgrades", "purchasables", item, "cost"], val => val + 5);
+			state.update('code', val => val - cost)
+
+		})
+
+
+		// return state.updateIn(["upgrades", "purchasables", item, "owned"], val => val + 1);
 	} else {
 		return state
 	}
-
 }
