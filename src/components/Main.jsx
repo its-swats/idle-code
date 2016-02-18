80139a2496a6bb94f64dad6d76188b0b44c1c75b
Shawn Watson <shawnw86@gmail.com>
2016-02-18 11:49:03 -0800

    Add initial UI for home screen and shop

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,13 +3,24 @@ import Shop from './Shop';
 import {connect} from 'react-redux' ;
 
 export const Main = React.createClass({
+  codePerSec: function () {
+  let second = this.props.player.get('totalSpeed') / 1000
+  let code = this.props.player.get('codePerTick')
+  return (Math.round(code / second * 10) / 10)
+  },
+
   render: function() {
     return (
-      <div><h1>Idle Coder</h1>
-      <p className='code'>{this.props.code}</p>
-      <p>{this.props.player.get('codeSpent')}</p>
-      <p>{this.props.player.get('totalSpeed')}</p>
-      <Shop state={this.props}/>
+      <div className='col-xs-12 col-sm-10 col-sm-offset-1 main'>
+        <div className='well'>
+          <h1 className='text-center'>Idle Coder</h1>
+          <ul className='list-inline text-center'>
+            <li className='code'>Lines of Code: {this.props.code}</li>
+            <li>Code/second: {this.codePerSec()}</li>
+            <li>Code Spent: {this.props.player.get('codeSpent')}</li>
+          </ul>
+          <Shop state={this.props}/>
+        </div>
       </div>
     )
   }
--- src/purchase_upgrade.jsx
+++ src/purchase_upgrade.jsx
@@ -1,12 +1,13 @@
 export function purchaseUpgrade(state, item) {
 	let cost = state.getIn(["upgrades", "purchasables", item, "cost"]);
+	let increment = state.getIn(["upgrades", "purchasables", item, "increment"]);
 	let updateType = state.getIn(["upgrades", "purchasables", item, "increase"]).toArray()[0]
 	let updateAmount = state.getIn(["upgrades", "purchasables", item, "increase"]).toArray()[1]
 	if (state.get("code") >= cost) {
 		return state.withMutations(state => {
 			state.updateIn(["player", "codeSpent"], val => val + cost);
 			state.updateIn(["upgrades", "purchasables", item, "owned"], val => val + 1);
-			state.updateIn(["upgrades", "purchasables", item, "cost"], val => val + 5);
+			state.updateIn(["upgrades", "purchasables", item, "cost"], val => val + increment);
 			state.update('code', val => val - cost)
 			state.updateIn(["player", updateType], val => val + updateAmount)
 		})
